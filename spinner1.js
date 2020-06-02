8946c4a0a27be041d3a23e722e46a0576a03b97d
ArpitaDeb <arpitadeb1990@gmail.com>
2020-06-02 03:16:11 +0000

    spinnert

---
+++ spinner1.js
@@ -0,0 +1,39 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+setTimeout(() => {
+  process.stdout.write('\r\|   '); 
+}, 1200);
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 2000);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 2300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, 3000);
+setTimeout(() => {
+  process.stdout.write('\r\|   '); 
+}, 3500);
+setTimeout(() => {
+  process.stdout.write("\n");
+}, 3700);
+
+// ... fill in the rest yourself ...
